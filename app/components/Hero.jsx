d8d86f7d6e2a6a4acec6287b3e27cc997e639ebd
Chanvicharo Ly <ly.cvc.ro@gmail.com>
2025-09-01 21:30:43 +0900

    adjust hero text and computer model

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -52,11 +52,11 @@ const Hero = () => {
         </div>
       </div>
 
-      <div className='paddingX absolute inset-0 xs:translate-y-15'>
+      <div className='paddingX absolute inset-0 xs:translate-y-15 translate-y-20'>
         <ComputersCanvas/>
       </div>
 
-      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+      <div className="absolute xs:-bottom-5 -bottom-10 w-full flex justify-center items-center">
         <a href="#about">
           <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-center p-2">
             <motion.div
